790cde0de4bf6e1a58f784c28ea7d67db8034874
Mr-ASbaloch <abdulsaeed1356@gmail.com>
2023-08-22 02:37:48 -0700

    commit

--- youtube-api/src/components/Header.jsx
+++ youtube-api/src/components/Header.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useContext } from "react";
+
+// react-router-dom
+import { Link, useLocation, useNavigate } from "react-router-dom";
+// react-icons
+import { slMenu } from "react-icons/sl";
+// images
+import ytlogomobile from "../Images/ytlogomobile.png";
+import ytlogo from "../Images/ytlogo.png";
+
+import { Context } from "../context/contextApi";
+import { useState } from "react";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [searchQuery, setSearchQuery] =useState("")
 
-export default Header
+  const {loading, mobileMenu,setMobileMenu} = useContext (Context)
+
+
+  return <div></div>;
+};
+
+export default Header;
