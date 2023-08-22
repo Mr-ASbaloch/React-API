787670abb6a1b38959e97945c6a2a90e25723580
Mr-ASbaloch <abdulsaeed1356@gmail.com>
2023-08-21 22:09:05 -0700

    commit

--- youtube-api/src/components/Navbar.jsx
+++ youtube-api/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Stack } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar
-
-    </div>
+   <Stack>
+    
+   </Stack>
   )
 }
 
